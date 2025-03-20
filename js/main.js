031604b58c6390eee5656f749e8e42e663a45257
AlejandroG2105 <alejandroprogramacionVS@gmail.com>
2025-03-20 15:33:53 -0600

    "Fin del curso"

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-numA = 30
-numB = 20
+// Funcion para sumar dos numeros
+function sumar (a, b){
+    return a + b; // Error intencional : deberia ser a + b
+}
 
-res = numB + numA
-console.log(res)
+// Usando la funcion para sumar dos numeros
+let numero1 = 5;
+let numero2 = 2;
+let numero3 = 6;
+
+// Llamada a la funcion y almacenamiento del resultado
+let resultado = sumar(numero1, numero2);
+
+// Mostrar el resultado
+console.log("El resultado de sumar" + numero1 + " y" + numero2 + "es:")
+
+Math.floor
